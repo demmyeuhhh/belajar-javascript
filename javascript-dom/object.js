f4af3fcc45b33d4ccbe5c13ad27ba90057fe164d
Demian <adityademian08@gmail.com>
2024-01-30 09:06:56 +0300

    object-dom

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -6,7 +6,16 @@ siswa.namaLengkap = function() {
     return this.namaDepan + " " + this.namaBelakang;
 };
 
+var siswa2 = {
+    namaDepan : "Alvin",
+    namaBelakang : "Sanjaya",
+    alamat : "Bandung",
+    namaLengkap : function() {
+        return this.namaDepan + " " + this.namaBelakang;
+    },
+};
 alert("Nama : " + siswa.namaLengkap());
+alert("Nama : " + siswa2.namaLengkap());
 
 //function namaLengkap() {
 
